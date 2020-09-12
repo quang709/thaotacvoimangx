ac0ebe200e428897b551af399c361a3147686e78
quang709 <69231857+quang709@users.noreply.github.com>
2020-09-12 14:44:45 +0700

    thao tac voi mang

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace thaotacvoimang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("In ra gia tri cua mang");
+            int[] numbers = new int[5];
+            numbers[0] = 2;
+            numbers[1] = 5;
+            numbers[2] = 9;
+            numbers[3] = 6;
+            numbers[4] = 7;
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[2]);
+            Console.WriteLine(numbers[3]);
+
+
+            Console.WriteLine("Tong mang la : {0}", tongmang(numbers));
+        }
+
+        private static int tongmang(int[] numbers)
+        {
+            int total = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                total = total + numbers[i];
+            }
+            return total;   
+        }
+    }
+}
+
+
